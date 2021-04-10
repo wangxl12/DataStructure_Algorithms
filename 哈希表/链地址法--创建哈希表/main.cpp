27121b76f0115144cba449c48856c47a4e52180a
wangxl12 <2750869379@qq.com>
2021-04-10 20:54:29 +0800

    链地址法--创建哈希表

--- 哈希表/链地址法--创建哈希表/main.cpp
+++ 哈希表/链地址法--创建哈希表/main.cpp
@@ -10,9 +10,9 @@ struct Link {
     struct Link *next;
 };
 
-typedef struct HASH {
+typedef struct {
     struct Link *link;
-};
+}HASH;
 
 int hashFunction(int key){
     /**
@@ -30,10 +30,10 @@ HASH *createHash() {
      * 创建哈希表，并将值初始化为-1
      */
     HASH *obj = (HASH *) (malloc(sizeof(HASH)));
-    obj->link = (struct Link *) (malloc(sizeof(struct Link) * base));
+    obj->link = (struct Link *) (malloc(sizeof(struct Link) * base));  // link数组中每一个值存放的是相应的链表长度
     // 初始化
     for(int i = 0;i < base; i++){
-        obj->link[i].val = -1;
+        obj->link[i].val = 0;
         obj->link[i].next = NULL;
     }
     return obj;
@@ -44,6 +44,7 @@ bool exists(HASH* obj, int key){
      * 判断哈希表中是否存在键key
      */
     struct Link* p = &obj->link[hashFunction(key)];
+    if(p->val == 0) return false;  // 如果该链表长度为0，则肯定不存在key
     for(;p != NULL; p = p->next){
         if(p->val == key) return true;
     }
@@ -53,25 +54,27 @@ void add(HASH* obj, int key){
     /**
      * 如果当前哈希表中有键值key，不进行操作，没就添加
      */
-     if(!exists(obj, key)){
-         struct Link* p = &obj->link[hashFunction(key)];
-         for(;p->next != NULL;p = p->next);
-         p->next = (struct Link* ) malloc(sizeof(struct Link));
-         p->next->val = key;
-         p->next->next = NULL;
-     }
+    if(!exists(obj, key)){
+        struct Link* p = &obj->link[hashFunction(key)];
+        p->val++;
+        for(;p->next != NULL;p = p->next);
+        p->next = (struct Link* ) malloc(sizeof(struct Link));
+        p->next->val = key;
+        p->next->next = NULL;
+    }
 }
 void del(HASH* obj, int key){
     /**
      * 如果当前哈希表中有键值key，删除
      */
-     if(exists(obj, key)){
-         struct Link* p = &obj->link[hashFunction(key)];
-         for(;p->next->val != key; p = p->next);
-         struct Link* temp = p->next;
-         p->next = p->next->next;
-         free(temp);
-     }
+    if(exists(obj, key)){
+        struct Link* p = &obj->link[hashFunction(key)];
+        p->val--;
+        for(;p->next->val != key; p = p->next);
+        struct Link* temp = p->next;
+        p->next = p->next->next;
+        free(temp);
+    }
 }
 void freeHash(HASH* obj){
     /**
@@ -86,21 +89,22 @@ void freeHash(HASH* obj){
         }
     }
     free(obj->link);
+    free(obj);
 }
 
 void display(HASH* obj){
     /**
      * 格式化输出哈希表中的内容
      */
-     struct Link* p = &obj->link[0];
-     for(int i = 0; i < base; i++){
-         struct Link* p = &obj->link[i];
-         while(p != NULL){
-             printf("%d -> ", p->val);
-             p = p->next;
-         }
-         printf("\n");
-     }
+    struct Link* p = &obj->link[0];
+    for(int i = 0; i < base; i++){
+        struct Link* p = &obj->link[i];
+        while(p != NULL){
+            printf("%d -> ", p->val);
+            p = p->next;
+        }
+        printf("\n");
+    }
     printf("\n");
 }
 
@@ -114,7 +118,7 @@ int main(){
     add(obj, 23);
     add(obj, 28);
     display(obj);
-    del(obj, -1);
+    del(obj, 12);
     display(obj);
     freeHash(obj);
     return 0;
